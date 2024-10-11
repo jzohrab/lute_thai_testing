3ede01d6373dce654878acd7a55e688863ad8ab6
Jeff Zohrab <jzohrab@gmail.com>
2024-10-11 13:26:05 -0700

    No tltk

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ texts = [
 เมื่อเวลา18.40 น. วันที่ 11 ต.ค.2567 ผู้สื่อข่าวรายงาน ในพื้นที่ ต.อัยเยอร์เวง อ.เบตง จ.ยะลา เกิดฝนตกหนัก ทำให้ดินสไลด์ทับเส้นทาง น้ำคลองกัดเซาะถนนภายในหมู่บ้านขาด น้ำหลากท่วมบ้านเรือนประชาชนหลายหลัง นอกจากนั้นยังเกิดดินสไลด์ปิดทับเส้นทางสาย 410 เบตง-ยะลา ช่วงกม.39."""
 ]
 
-sentence_engines = [ "whitespace+newline", "whitespace", "newline", "tltk", "thaisum", "wtp", "wtp-tiny" ]
+sentence_engines = [ "whitespace+newline", "whitespace", "newline", "thaisum", "wtp", "wtp-tiny" ]
 # default engine ("") not available as it uses pycrfsuite, which is an extra dependency.  Skipping
+# "tltk" tltk is extra dependency, has build deps so might cause trouble -- on mac, ERROR: Command errored out with exit status 1:
 
-word_engines = [ "newmm", "newmm-safe", "longest", "tltk" ]
+word_engines = [ "newmm", "newmm-safe", "longest" ]
 
 # "attacut" word engine not available due to "ERROR: Could not find a version that satisfies the requirement torch>=1.2.0 (from attacut) (from versions: none)"
-
-# tltk is extra dependency
+# "tltk" tltk is extra dependency, has build deps so might cause trouble -- on mac, ERROR: Command errored out with exit status 1:
 
 for text in texts:
     print("=" * 50)
